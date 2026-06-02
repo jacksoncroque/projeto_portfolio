098b6e7ea583b9982ba63fed149fb48a838537d5
jacksoncroque <jackson.crq@gmail.com>
2026-06-02 18:31:05 -0300

    Agora sim, finalização do hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,4 +1,7 @@
 import { ArrowRightIcon } from "lucide-react";
+import { FiGithub } from "react-icons/fi";
+import { SlSocialLinkedin } from "react-icons/sl";
+import { IoNewspaperOutline } from "react-icons/io5";
 
 import styles from "./Hero.module.scss";
 import { ConstJackson } from "../ConstJackson/ConstJackson";
@@ -27,6 +30,38 @@ const Hero = () => {
             <ArrowRightIcon />
           </button>
           <button>Entrar em contato</button>
+        </div>
+        <div className={styles.containerLeftContact}>
+          <div className={styles.containerLeftContactIcons}>
+            <FiGithub />{" "}
+            <a
+              href="https://github.com/jacksoncroque"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+          </div>
+          <div className={styles.containerLeftContactIcons}>
+            <SlSocialLinkedin />{" "}
+            <a
+              href="https://linkedin.com/in/jacksoncrq"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Linkedin
+            </a>
+          </div>
+          <div className={styles.containerLeftContactIcons}>
+            {" "}
+            <IoNewspaperOutline />{" "}
+            <a
+              href="public/docs/Jackson_Coelho_Dev.pdf"
+              download={"Jackson_Coelho_Dev"}
+            >
+              Currículo
+            </a>
+          </div>
         </div>
       </div>
       <div className={styles.containerRight}>
