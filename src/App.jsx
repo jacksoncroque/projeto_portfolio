025758962ac0337ba9053017e1d2aedfa1c8dcb6
jacksoncroque <jackson.crq@gmail.com>
2026-06-05 23:15:39 -0300

    Adicionado footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Projects from "./Components/Projects/Projects";
 import About from "./Components/About/About";
 import Stacks from "./Components/Stacks/Stacks";
 import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <About/>
       <Stacks />
       <Contact />
+      <Footer />
     </>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.containerWrapper}>
+        <div>
+          <p>&copy; {new Date().getFullYear()} Jackson</p>
+          <p>Todos os direitos reservados</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
